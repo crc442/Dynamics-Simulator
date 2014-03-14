be3e6b4b804d4283c5764eb926f192426d7c0010
crc442 <EequalsMCsquare91@gmail.com>
2014-03-13 20:47:12 -0500

    get it clicked

--- CMDBUT2.C
+++ CMDBUT2.C
@@ -14,3 +14,26 @@ struct button
 	char name[10];
 };
 
+clicked(struct button *obj)
+{
+	int top,left,right,bottom;
+
+	top=(*obj).y-10;
+	left=(*obj).x-10;
+	right=(*obj).x+65;
+	bottom=(*obj).y+10;
+
+	setfillstyle(SOLID_FILL,LIGHTGRAY);
+	bar(left+8,top+8,right+8,bottom+8);
+
+	setfillstyle(1,LIGHTGRAY);
+	bar(left,top,right,bottom);
+
+	setfillstyle(1,GREEN);
+	bar(left+8,top,right+8,bottom);
+
+	setcolor(WHITE);
+	settextstyle(DEFAULT_FONT,HORIZ_DIR,1);
+	outtextxy(left+12,top+8,(*obj).name);
+	return 0;
+}
