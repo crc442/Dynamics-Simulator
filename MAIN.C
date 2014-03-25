ef6f0c8dbb9cd85f24aad192ac809fc91a8d7c83
crc442 <EequalsMCsquare91@gmail.com>
2014-03-25 05:14:17 -0500

    i guess

--- MAIN.C
+++ MAIN.C
@@ -7,3 +7,9 @@
 #include "CMDBUT2.C"
 #include "TXTBOX.C"
 
+#define MX 40
+#define MY 30
+#define VX1 20      //Coordinates within which simulation is done
+#define VY1 50
+#define VX2 xm-80
+#define VY2 ym-50
