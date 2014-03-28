572e04368bf88525ed0034479724f1fb4b14833f
crc442 <EequalsMCsquare91@gmail.com>
2014-03-27 22:14:17 -0500

    things i need

--- MAIN.C
+++ MAIN.C
@@ -14,3 +14,21 @@
 #define VX2 xm-80
 
 #define VY2 ym-50
+
+struct act_vir
+{
+	float x;
+	float y;
+};
+struct act_vir *av;
+
+struct vir_act
+{
+	int x;
+	int y;
+};
+struct vir_act *va;
+
+struct virt_co *actual_vir(int x, int y);
+struct virt_co *vir_actual(float x, float y);
+struct virt_co *actual_vir1(int x, int y);
